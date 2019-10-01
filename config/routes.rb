09f84ec54be970176979ae1a1371f09a2ccea521
Handan Zhang <hmix.cc@gmail.com>
2019-10-01 15:53:54 +0200

    changes to 404 and 500 page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+  get "/404", to: 'errors#not_found'
+  get "/500", to: 'errors#internal_error'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
