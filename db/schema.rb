34840555ad823e915389bf18c6b24428d5c6000b
Tulasi ratna <ratna.tulasi341@gmail.com>
2019-10-02 12:40:41 +0200

    users and meetings created

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_01_105550) do
+ActiveRecord::Schema.define(version: 2019_10_02_092339) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,9 @@ ActiveRecord::Schema.define(version: 2019_10_01_105550) do
     t.integer "limitation"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
+    t.text "description"
+    t.string "image"
   end
 
   create_table "meetings", force: :cascade do |t|
