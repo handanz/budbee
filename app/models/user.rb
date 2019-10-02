44d0a42f9bb56d31e6be5289f61c4a69b8d3c868
Handan Zhang <hmix.cc@gmail.com>
2019-10-02 15:20:03 +0200

    added username, firstname, and lastname to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,13 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  :recoverable, :rememberable, :validatable
+
+  validates :email, uniqueness: true
+  validates :username, uniqueness: true
+
+  # use this instead of email_changed? for Rails = 5.1.x
+  def will_save_change_to_email?
+    false
+  end
 end
