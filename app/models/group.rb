b22cd20d6a45152e22add1840f1ce151df555cde
Handan Zhang <hmix.cc@gmail.com>
2019-10-02 16:23:48 +0200

    added new tags to seed file

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,5 +1,6 @@
 class Group < ApplicationRecord
   include PgSearch::Model
+  acts_as_taggable_on :tags
   pg_search_scope :global_search,
     against: [ :name, :description, :city ],
     using: {
