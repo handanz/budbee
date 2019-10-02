c7844fa3b02b6221831e55c93a451b0c813174bf
Tulasi ratna <ratna.tulasi341@gmail.com>
2019-10-02 15:22:34 +0200

    few lines removed in group.rb file

--- app/models/group.rb
+++ app/models/group.rb
@@ -2,9 +2,6 @@ class Group < ApplicationRecord
   include PgSearch::Model
   pg_search_scope :global_search,
     against: [ :name, :description, :city ],
-    # associated_against: {
-    #   tags_list: [ :first_name, :last_name ]
-    # },
     using: {
       tsearch: { prefix: true }
     }
