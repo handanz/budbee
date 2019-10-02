1444f31c44f3606315fc4ff396764f541d9c3aca
Handan Zhang <hmix.cc@gmail.com>
2019-10-02 13:51:10 +0200

    added acts as tags gem

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  acts_as_taggable_on :tags
+  has_many :groups
+  has_many :meetings, through: :groups
 end
