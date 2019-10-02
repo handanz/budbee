5e696530d93bd4f9151235c541caba079b429863
Tulasi ratna <ratna.tulasi341@gmail.com>
2019-10-02 15:03:16 +0200

    pg_search added on group.rb file

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,2 +1,11 @@
 class Group < ApplicationRecord
+  include PgSearch::Model
+  pg_search_scope :global_search,
+    against: [ :name, :description, :city ],
+    # associated_against: {
+    #   tags_list: [ :first_name, :last_name ]
+    # },
+    using: {
+      tsearch: { prefix: true }
+    }
 end
