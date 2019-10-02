33840872d8f1f6cc4194b93043d9afcd89db7efb
Handan Zhang <hmix.cc@gmail.com>
2019-10-02 17:22:03 +0200

    added routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,9 @@ Rails.application.routes.draw do
   get "/404", to: 'errors#not_found'
   get "/500", to: 'errors#internal_error'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :groups, only: [ :show, :index ] do
+    resources :meetings, only: [ :show ]
+  end
+
 end
