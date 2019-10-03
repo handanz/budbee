909920e447b10e855b80b11aa427fa4f7b4d1a54
Handan Zhang <hmix.cc@gmail.com>
2019-10-03 17:54:22 +0200

    pg search works for name, description, but not tags

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,12 +1,10 @@
 class Group < ApplicationRecord
+  include PgSearch::Model
   has_many :meetings
   has_many :user_groups
   has_many :users, through: :user_groups
-
-  include PgSearch::Model
-  has_many :meetings
-  has_many :users, through: :user_groups
   acts_as_taggable_on :tags
+
   pg_search_scope :global_search,
     against: [:name, :description, :city],
     using: {
