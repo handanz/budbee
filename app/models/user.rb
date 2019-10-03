70b6c819c592f5d592059aaed49e0bc9ff1f4408
Handan Zhang <hmix.cc@gmail.com>
2019-10-03 10:25:21 +0200

    added has_many relations to group, meeting, and user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  has_many :groups
+  has_many :meetings
+
   acts_as_taggable_on :tags
   devise :database_authenticatable, :registerable,
   :recoverable, :rememberable, :validatable
