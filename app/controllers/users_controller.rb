397661487a92b016d3fd5f02705418c93fa36fcd
Andreas Holmbäck <holmbackandreas@gmail.com>
2019-10-04 15:25:52 +0200

    profilepage styling

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,5 +7,7 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @groups = @user.groups
     @meetings = @user.meetings
+    @meeting = @meetings.first
+    @interests = @user.tag_list
   end
 end
