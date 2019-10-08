aee3b736827deb0f3601fef6d29678c994318bfe
Tulasi ratna <ratna.tulasi341@gmail.com>
2019-10-08 12:40:34 +0200

    model and controller generated

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,5 @@
+class ArticlesController < ApplicationController
+  def show
+    @user = User.find(params[:id])
+  end
+end
---
+++ db/migrate/20191008102856_create_articles.rb
@@ -0,0 +1,12 @@
+class CreateArticles < ActiveRecord::Migration[5.2]
+  def change
+    create_table :articles do |t|
+      t.string :title
+      t.text :content
+      t.string :image
+      t.references :user, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
