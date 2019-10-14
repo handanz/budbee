d311e2cffccdcc8642bd7bd4b74b3903fca07f31
Handan Zhang <hmix.cc@gmail.com>
2019-10-14 08:53:02 +0200

    added new article feature and did basic style of the new article page

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,4 +5,21 @@ class ArticlesController < ApplicationController
     @user = @article.user
     @tags = @article.tag_list
   end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+    if @article.save
+      redirect_to article_path(@article)
+    else
+      render :new
+    end
+  end
+
+  def article_params
+    params.require(:article).permit(:title, :content, :image)
+  end
 end
